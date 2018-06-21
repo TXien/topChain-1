380b8bfe4865bd7061806e79094a78be44cb250e
zomlandlin <zomland.lin@imediplus.com>
2018-06-21 18:16:15 +0800

    sue version

---
+++ run.py
@@ -0,0 +1,19 @@
+
+
+
+#getBlockNumber
+#if(blockNumber == null):
+#genesisBlock
+#genesisBlockInsertToDB
+#updateGenesisAccountBalance
+#else:
+#getPendingTransaction
+#verifyTransaction
+#pushTransactionToArray
+#getparentblock
+#newBlock_POA(blockData,parentblock,key)
+#blockStoreToDB
+#updateDBAccountStatus
+#pack transaction
+#stay time
+
